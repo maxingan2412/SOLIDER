6ef191f99bff7387fe8b1157cfdbc5d7bdd614d0
maxingan2412 <maxingan1993@gmail.com>
2023-09-28 14:51:35 +0200

    1

--- run_solider.sh
+++ run_solider.sh
@@ -9,6 +9,7 @@ python -W ignore -m torch.distributed.launch --nproc_per_node=8 main_solider.py 
 --global_crops_scale 0.8 1. \
 --local_crops_scale 0.05 0.8 \
 --partnum 3 \
+
 --parthead_nlayers 3 \
 --semantic_loss 1.0 \
 # if there's ifislfjalsfsa
